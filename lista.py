6b0fa51899e28b64c4877fbda95c7bedf08b526b
dboguerin <danielboguerin@gmail.com>
2021-12-13 16:45:15 -0600

    agrega funcionalidad de cargar contactos por xml

--- lista.py
+++ lista.py
@@ -1,20 +1,52 @@
 class nodo:
-    def __init__(self, numero = None, nombre= None, siguiente = None):
+    def __init__(self, numero = None, nombre= None, apellido = None,siguiente = None):
         self.numero = numero
         self.nombre = nombre
+        self.apellido = apellido
         self.siguiente = siguiente
 
 class lista: 
     def __init__(self):
         self.cabeza = None
 
-    def añadir_inicio(self, numero, nombre):
-        self.cabeza = nodo(numero=numero,nombre=nombre, siguiente=self.cabeza)  
+    def añadir_inicio(self, numero, nombre, apellido):
+        self.cabeza = nodo(numero=numero,nombre=nombre,apellido=apellido, siguiente=self.cabeza)  
+    
+    #def es_vacia(self):
+# return self.cabeza == None
+
+#def obtener_ultimo(self):
+# temp = self.cabeza
+# while(temp.siguiente is not None):
+# temp = temp.siguiente
+# return temp.numero
+
+#def borrar(self, key):
+# actual = self.cabeza
+# anterior = None
+# while actual and actual.numero != key:
+# anterior = actual
+# actual = actual.siguiente
+# if anterior is None:
+# self.cabeza = actual.siguiente
+# elif actual:
+# anterior.siguiente = actual.siguiente
+# actual.siguiente = None
+
+    def verificarRepetido( self, x ):
+        contador=1
+        nodo = self.cabeza
+        while nodo != None:
+            if nodo.numero == x:
+                return True
+            else:
+                nodo = nodo.siguiente
+        return False
 
     def imprimir( self ):
         contador=1
         nodo = self.cabeza
         while nodo != None:
-            print(str(contador)+") "+nodo.nombre+": "+str(nodo.numero)+"\n")
+            print(str(contador)+") "+nodo.nombre+" "+nodo.apellido+": "+str(nodo.numero)+"\n")
             contador+=1
             nodo = nodo.siguiente
--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import time,os,lista
+from xml.dom import minidom
 
 clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 numeros=lista.lista()
@@ -6,14 +7,15 @@ def menu():
     clearConsole()
     print("1. Ingresar nuevo numero")
     print("2. Listar numeros de telefono guardados")
-    print("3. Salida")
+    print("3. Cargar XML")
     opcion_seleccionada=input("Que desea hacer?\n")
 
     if opcion_seleccionada=="1":
         clearConsole()
         numero=input("Ingrese el numero de telefono:\n")
         nombre=input("Ingrese el nombre para el numero de telefono:\n")
-        numeros.añadir_inicio(numero, nombre)
+        apellido=input("Ingrese el apellido: \n")
+        numeros.añadir_inicio(numero, nombre, apellido)
         print("Regresando al menu principal...")
         time.sleep(3)
         clearConsole()
@@ -27,7 +29,31 @@ def menu():
         menu()
     elif opcion_seleccionada=="3":
         clearConsole()
-        print("Adios...")
+        docXML= minidom.parse(input("Ingrese la ruta del archivo XML: \n"))
+        contactos= docXML.getElementsByTagName("contactos")
+        contador=1 
+        for x in contactos:
+            print("Cargando contacto No. "+str(contador))
+            telefono=x.getElementsByTagName("telefono")
+            telefono_valor=telefono[0].firstChild.nodeValue.strip('"')
+            #print("Artista: "+artista_valor)
+            nombre=x.getElementsByTagName("nombre")
+            nombre_valor=nombre[0].firstChild.nodeValue.strip('"')
+            apellido=x.getElementsByTagName("apellido")
+            apellido_valor=apellido[0].firstChild.nodeValue.strip('"')
+            if numeros.verificarRepetido(telefono_valor):
+                print("El contacto No. "+str(contador)+" ya es repetido, no se agrego...")
+                time.sleep(3)
+                contador+=1
+                continue
+            else:
+                numeros.añadir_inicio(telefono_valor, nombre_valor, apellido_valor)
+                contador+=1
+        print("Regresando al menu principal...")
+        time.sleep(3)
+        clearConsole()
+        menu()
+
     else:
         print("porfavor elija una opcion valida")
         time.sleep(2)
